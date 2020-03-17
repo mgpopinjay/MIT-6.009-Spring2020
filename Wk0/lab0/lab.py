d502c8331a8e57320dfb6067a14f02a8b4465aec
phil <ptinn@mit.edu>
2020-03-17 16:38:56 -0400

    Wk0 Pset

---
+++ Wk0/lab0/lab.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+
+import math
+
+from PIL import Image as Image
+
+# NO ADDITIONAL IMPORTS ALLOWED!
+
+
+def get_pixel(image, x, y):
+    return image['pixels'][(x * image['width'] + y)]
+
+
+def set_pixel(image, x, y, c):
+
+    image['pixels'].append(c)
+
+
+def apply_per_pixel(image, func):
+    result = {
+        'height': image['height'],
+        'width': image['width'],
+        'pixels': [],
+    }
+    print("image:", image)
+    for x in range(image['height']):
+        for y in range(image['width']):
+            pixel = get_pixel(image, x, y)
+            # print("pixel:", pixel)
+            newcolor = func(pixel)
+            # print("new pixel:", newcolor)
+            set_pixel(result, x, y, newcolor)
+    return result
+
+
+def inverted(image):
+    return apply_per_pixel(image, lambda c: 255-c)
+
+
+# HELPER FUNCTIONS
+
+def correlate(image, kernel):
+    """
+    Compute the result of correlating the given image with the given kernel.
+
+    The output of this function should have the same form as a 6.009 image (a
+    dictionary with 'height', 'width', and 'pixels' keys), but its pixel values
+    do not necessarily need to be in the range [0,255], nor do they need to be
+    integers (they should not be clipped or rounded at all).
+
+    This process should not mutate the input image; rather, it should create a
+    separate structure to represent the output.
+
+    DESCRIBE YOUR KERNEL REPRESENTATION HERE
+    """
+    raise NotImplementedError
+
+
+def round_and_clip_image(image):
+    """
+    Given a dictionary, ensure that the values in the 'pixels' list are all
+    integers in the range [0, 255].
+
+    All values should be converted to integers using Python's `round` function.
+
+    Any locations with values higher than 255 in the input should have value
+    255 in the output; and any locations with values lower than 0 in the input
+    should have value 0 in the output.
+    """
+    raise NotImplementedError
+
+
+# FILTERS
+
+def blurred(image, n):
+    """
+    Return a new image representing the result of applying a box blur (with
+    kernel size n) to the given input image.
+
+    This process should not mutate the input image; rather, it should create a
+    separate structure to represent the output.
+    """
+    # first, create a representation for the appropriate n-by-n kernel (you may
+    # wish to define another helper function for this)
+    raise NotImplementedError
+
+    # then compute the correlation of the input image with that kernel
+    raise NotImplementedError
+
+    # and, finally, make sure that the output is a valid image (using the
+    # helper function from above) before returning it.
+    raise NotImplementedError
+
+
+
+# HELPER FUNCTIONS FOR LOADING AND SAVING IMAGES
+
+def load_image(filename):
+    """
+    Loads an image from the given file and returns a dictionary
+    representing that image.  This also performs conversion to greyscale.
+
+    Invoked as, for example:
+       i = load_image('test_images/cat.png')
+    """
+    with open(filename, 'rb') as img_handle:
+        img = Image.open(img_handle)
+        img_data = img.getdata()
+        if img.mode.startswith('RGB'):
+            pixels = [round(.299 * p[0] + .587 * p[1] + .114 * p[2])
+                      for p in img_data]
+        elif img.mode == 'LA':
+            pixels = [p[0] for p in img_data]
+        elif img.mode == 'L':
+            pixels = list(img_data)
+        else:
+            raise ValueError('Unsupported image mode: %r' % img.mode)
+        w, h = img.size
+        return {'height': h, 'width': w, 'pixels': pixels}
+
+
+def save_image(image, filename, mode='PNG'):
+    """
+    Saves the given image to disk or to a file-like object.  If filename is
+    given as a string, the file type will be inferred from the given name.  If
+    filename is given as a file-like object, the file type will be determined
+    by the 'mode' parameter.
+    """
+    out = Image.new(mode='L', size=(image['width'], image['height']))
+    out.putdata(image['pixels'])
+    if isinstance(filename, str):
+        out.save(filename)
+    else:
+        out.save(filename, mode)
+    out.close()
+
+
+if __name__ == '__main__':
+    # code in this block will only be run when you explicitly run your script,
+    # and not when the tests are being run.  this is a good place for
+    # generating images, etc.
+
+    img = load_image('test_images/bluegill.png')
+    inverted = inverted(img)
+    save_image(inverted, 'test_images/bluegill_inverted.png')
